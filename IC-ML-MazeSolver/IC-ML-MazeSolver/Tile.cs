56306ee4c975e103c07d26938990612f0bef880e
Ian Cichy <ICichy@uwcu.org>
2018-01-12 17:00:44 -0600

    Updates

---
+++ IC-ML-MazeSolver/IC-ML-MazeSolver/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IC_ML_MazeSolver
+{
+    public class Tile
+    {
+        public int xLoc { get; set; }
+        public int yLoc { get; set; }
+        public int type { get; set; }
+        public Button btn { get; set; }
+
+        public Tile(int x, int y, int t)
+        {
+            btn = new Button();
+            xLoc = x;
+            yLoc = y;
+            type = t;
+        }
+    }
+}
